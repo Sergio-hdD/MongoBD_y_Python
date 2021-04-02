bc17e087d98250df5f8ca39eb9cbc473063d469c
Sergio-hdD <sergio-hdd@hotmail.com>
2021-04-02 13:07:50 -0300

    6- Buscar documentos filtrando por uno o más campos

--- Primer_Crud/index.py
+++ Primer_Crud/index.py
@@ -28,19 +28,60 @@ except mg.errors.ConnectionFailure as errorDeConexion: #Si no se puede conectar
     print("Fallo de conexión: "+errorDeConexion)
 #----------------Fin conexión al servidor---------------------
 
-def mostrarDatos():
-    for documento in coleccion.find():
+''' 
+#OPCIÓN 1
+def mostrarDatos(nombre = "", sexo = "", calificacion = "" ): #Al ponerlos así los parámetros son opcionales, es decir si en algún lado no se los pasa no tira error y estarán vacios 
+    objetoABuscar = {} #Inicia como diccionario vacío
+    if len(nombre) != 0 : #Si no está vacío lo agrego al diccionario
+        objetoABuscar["nombre"] = nombre
+    if len(sexo) != 0 :
+        objetoABuscar["sexo"] = sexo
+    if len(calificacion) != 0 :
+        objetoABuscar["calificacion"] = calificacion
+
+    for documento in coleccion.find(objetoABuscar): #Si el objetoABuscar llegara vacío, el find buscaría sin tener en cuenta algún criterio (traería todo)
         tabla.insert('', 0, text = documento["_id"], values = documento["nombre"] )  #El primer parámetro es un registro padre, en este caso no tienen (todos serán padres), segundo un indice y los demás los valores
     cliente.close() #Cierro la conexión
+
+def buscarDocumentoAlumno():
+    limparPantalla()
+    mostrarDatos(buscarPorNombre.get(), buscarPorSexo.get(), buscarPorCalificacion.get())
+# FIN OPCIÓN 1
+'''
+# OPCIÓN 2
+def mostrarDatos(objetoABuscar = {}): #Al ponerlo así el parámetro es opcional, es decir si en algún lado no se lo pasa no tira error y estará vacio 
+    for documento in coleccion.find(objetoABuscar): #Si el objetoABuscar llegara vacío, el find buscaría sin tener en cuenta algún criterio (traería todo)
+        tabla.insert('', 0, text = documento["_id"], values = documento["nombre"] )  #El primer parámetro es un registro padre, en este caso no tienen (todos serán padres), segundo un indice y los demás los valores
+    cliente.close() #Cierro la conexión
+
+def buscarDocumentoAlumno():
+    objetoABuscar = {} #Inicia como diccionario vacío
+    nombre = buscarPorNombre.get() 
+    sexo = buscarPorSexo.get()
+    calificacion = buscarPorCalificacion.get()
+    if len(nombre) != 0 : #Si no está vacío lo agrego al diccionario
+        objetoABuscar["nombre"] = nombre
+    if len(sexo) != 0 :
+        objetoABuscar["sexo"] = sexo
+    if len(calificacion) != 0 :
+        objetoABuscar["calificacion"] = calificacion
+    
+    if objetoABuscar == {} : #Si todos los campos de texto están vacíos
+        messagebox.showerror(message = "Debe ingresar al menos un dato para la búsqueda")
+    else: #Si se ingresa al menos un criterio de busqueda    
+        limparPantalla()
+        mostrarDatos(objetoABuscar)
+#FIN OPCIÓN 2
 
 def limparPantalla():
     for registro in tabla.get_children():
         tabla.delete( registro )  #Borro cada registro que se está mostrando en la tabla
 
-def crearInputAndLabel(texto, labelRow, labelColumn, botonRow, botonColumn):
-    Label(ventana, text = texto ).grid(row = labelRow, column = labelColumn) # Label(dónde va as estar, texto).grid(ubicación)
+def crearInputAndLabel(texto, labelRow, labelColumn, campoTextoRow, campoTextoColumn):
+    Label(ventana, text = texto ).grid(row = labelRow, column = labelColumn, sticky = W+E) # Label(dónde va as estar, texto).grid(ubicación)
+    #sticky = W+E, que abarque todo de izquierda a derecha 
     varEntry = Entry(ventana) #Va a estar en la ventana
-    varEntry.grid(row = botonRow, column = botonColumn) # La posisión del input
+    varEntry.grid(row = campoTextoRow, column = campoTextoColumn, sticky = W+E) # La posisión del input
     return varEntry
 
 def limpiarInputs(): #Saca de los input que fueron agregados al tocar el boton "Agregar alumno" 
@@ -118,6 +159,7 @@ tabla.bind("<Double-Button-1>", dobleClicTabla) #Cuando se da doble clic llama a
 
 #Nombre
 nombre = crearInputAndLabel("Nombre", 2, 0, 2, 1)
+nombre.focus() #Para que cuando inicie la ventana el cursor esté en este campo de texto
 #Sexo
 sexo = crearInputAndLabel("Sexo", 3, 0, 3, 1)
 #Calificación
@@ -125,17 +167,27 @@ calificacion = crearInputAndLabel("Calificación", 4, 0, 4, 1)
 
 #Botón agregar
 agregar = Button(ventana,text = "Agregar alumno", command = agregarDocumentoAlumno, bg = "green", fg = "white") #Command llama a la función al tocar el botón, bg es el fondo, fg la letra 
-agregar.grid(row = 5, columnspan = 2) 
+agregar.grid(row = 5, columnspan = 2, sticky = W+E) #sticky = W+E, que abarque todo de izquierda a derecha   
 
 #Botón editar 
 editar = Button(ventana,text = "Editar alumno", command = editarDocumentoAlumno, bg = "yellow") 
-editar.grid(row = 6, columnspan = 2) 
+editar.grid(row = 6, columnspan = 2, sticky = W+E) 
 editar["state"] = "disabled" #El botón arranca en estado disabled
 
 #Botón borrar 
 borrar = Button(ventana,text = "Borrar alumno", command = borrarDocumentoAlumno, bg = "red", fg = "white") 
-borrar.grid(row = 7, columnspan = 2)
+borrar.grid(row = 7, columnspan = 2, sticky = W+E)
 borrar["state"] = "disabled" #El botón arranca en estado disabled
+
+#Buscar por nombre
+buscarPorNombre = crearInputAndLabel("Buscar por nombre", 8, 0, 8, 1)
+#Buscar por sexo
+buscarPorSexo = crearInputAndLabel("Buscar por sexo", 9, 0, 9, 1)
+#Buscar por calificación
+buscarPorCalificacion = crearInputAndLabel("Buscar por calificación", 10, 0, 10, 1)
+#Botón buscar 
+buscar = Button(ventana,text = "Buscar alumno", command = buscarDocumentoAlumno, bg = "blue", fg = "white") 
+buscar.grid(row = 11, columnspan = 2, sticky = W+E) 
 
 mostrarDatos()
 ventana.mainloop() #Ciclo principal
